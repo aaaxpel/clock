832294b4c610136e1855660b4434f581ab61ce5b
axpel <sqlc@mail.ru>
2025-07-28 10:08:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: '/clock/',
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
+    outDir: 'dist',
+    emptyOutDir: true
   },
 });
