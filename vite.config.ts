0a6ac612aeeeefd9630293291964061d523ecb07
axpel <sqlc@mail.ru>
2025-07-28 10:35:25 +0700

    more tweaking

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: 'https://github.com/aaaxpel/clock/',
+  base: '/clock/',
   server: {
     port: 3000,
   },
